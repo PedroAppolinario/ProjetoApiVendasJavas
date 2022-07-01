28441b65f4fc426aa19f9720a339b5947b1df56d
Pedro Appolinario <pedro.appolinario@dmslog.com>
2022-07-01 19:53:36 -0300

    first commit

---
+++ src/main/java/pedro/com/github/ApiVendasJava/ApiVendasJavaApplication.java
@@ -0,0 +1,13 @@
+package pedro.com.github.ApiVendasJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVendasJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVendasJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pedro/com/github/ApiVendasJava/ApiVendasJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package pedro.com.github.ApiVendasJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVendasJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
